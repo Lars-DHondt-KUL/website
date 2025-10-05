14b49514ccfad96e5145dbf5b4502de87786f731
Lars D'Hondt <71920801+Lars-DHondt-KUL@users.noreply.github.com>
2025-10-05 10:38:49 +0200

    dummy pages source

---
+++ source/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'website'
+copyright = "2025, Lars D'Hondt"
+author = "Lars D'Hondt"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_theme_options = {
+    'fixed_sidebar': 'true', 
+    'sidebar_width': '250px',
+}
+
+html_sidebars = {
+    '**': ['navigation.html', 'searchfield.html'],
+}
+
+html_static_path = ['_static']
+html_logo = ''
+html_favicon = ''
---
+++ source/index.rst
@@ -0,0 +1,14 @@
+
+
+PredSim
+========
+
+.. toctree::
+   :maxdepth: 1
+
+   About-me
+   linkedin <https://www.linkedin.com/in/lars-dhondt/>
+   orcid <https://orcid.org/0000-0002-2971-7297>
+   github <https://github.com/Lars-DHondt-KUL>
+
+
